1a675024ffb6574cdaf8fe586fc9fc42d11ff4c0
LIASBDLIASHH9Q3PHB412B3412IB3123 <43847033+LIASBDLIASHH9Q3PHB412B3412IB3123@users.noreply.github.com>
2018-12-07 13:27:11 -1200

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const Yushi = require('./handle/Yushi');
+const client = new Yushi({
+  fetchAllMembers: true,
+  disabledEvents: ["TYPING_START", "USER_NOTE_UPDATE"],
+  disableEveryone: true
+});
+
+require('./handle/events')(client);
+require('./handle/module')(client);
+
+client.login(process.env.TOKEN);
