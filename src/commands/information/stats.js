45237395808cd9701a643484de68c0901f56123c
LIASBDLIASHH9Q3PHB412B3412IB3123 <43847033+LIASBDLIASHH9Q3PHB412B3412IB3123@users.noreply.github.com>
2018-12-07 13:54:39 -1200

    Update stats.js

--- src/commands/information/stats.js
+++ src/commands/information/stats.js
@@ -1,10 +1,30 @@
 const Discord = require('discord.js');
 
 exports.run = async (client, message, args) => {
+  
+  const uptime = parseDur(client.uptime);
   const stats = new Discord.RichEmbed()
+  .setColor("RED")
   .setAuthor(`${client.user.tag} Status`)
   .setThumbnail(`${client.user.displayAvatarURL}`)
+  .addField(`Uptime: ${uptime}`)
   message.channel.send(stats)
+}
+
+function parseDur (ms){
+	let seconds = ms / 1000;
+	let days = parseInt(seconds / 86400);
+	seconds = seconds % 86400;
+	let hours = parseInt(seconds / 3600);
+	seconds = seconds % 3600;
+	let minutes = parseInt(seconds / 60);
+	seconds = parseInt(seconds % 60);
+	let fin = [];
+	if(days) fin.push(`${days}d`);
+	if(hours) fin.push(`${hours}h`);
+	if(minutes) fin.push(`${minutes}m`);
+	fin.push(`${seconds}s`);
+	return fin.join(' ');
 }
 
 exports.conf = {
