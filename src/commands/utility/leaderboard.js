593be1d01fa90484ad0341dc932b6acbb9257d22
LIASBDLIASHH9Q3PHB412B3412IB3123 <43847033+LIASBDLIASHH9Q3PHB412B3412IB3123@users.noreply.github.com>
2018-12-09 19:02:27 +0700

    Create leaderboard.js

---
+++ src/commands/utility/leaderboard.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js'), 
+      arraySort = require('array-sort');
+
+this.run = async (client, message, args, color) => {
+
+    let invites = await message.guild.fetchInvites().catch(error => { 
+        return message.channel.send({embed: { color: 0xFF0000, description: 'Sorry, I don\'t have the proper permissions to view invites!' }});
+    }) 
+
+    invites = invites.array();
+
+    arraySort(invites, 'uses', { reverse: true }); 
+
+    let possibleinvites = [];
+    let index = 0;
+    invites.forEach(function(invites) {
+        possibleinvites.push(`**${++index}**. » **${invites.inviter.tag}** 》 \`${invites.uses}\` **invites**`)
+    })
+
+    const embed = new Discord.RichEmbed()
+        .setTitle(`🎖 INVITE LEADERBOARD 🎖`)
+        .setColor(color)
+        .setThumbnail(message.guild.iconURL)
+        .setDescription(`${possibleinvites.join('\n')}`)
+        .setTimestamp(`• Message For ${message.author.tag}`, message.author.displayAvatarURL);
+    message.channel.send(embed);
+    
+}
+
+this.conf = {
+  aliases: ['invites'],
+  cooldown: '5'
+}
+
+this.help = {
+  name: 'leaderboard',
+  description: 'Show leaderboard invite in server',
+  example: 'leaderboard'
+}
