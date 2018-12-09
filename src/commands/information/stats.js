b5c793d841a15b553b58d37bd6050f1645e63ecb
LIASBDLIASHH9Q3PHB412B3412IB3123 <43847033+LIASBDLIASHH9Q3PHB412B3412IB3123@users.noreply.github.com>
2018-12-09 17:56:59 +0700

    Update stats.js

--- src/commands/information/stats.js
+++ src/commands/information/stats.js
@@ -20,8 +20,7 @@ exports.run = async (client, message, args) => {
   .addField("System Status:", `Discord.js : v${version}\nNode : ${process.version}\nBot Version : 0.0.1-Beta\nMemor Usage : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\nCPU : ${os.cpus().map(i => i.model)[0]}\nArch : ${os.arch()}\nPlatform : ${os.platform()}`, true)
    .addField('About me', '**Yushi Nishimaro bot created in Indonesian , High Quality Bot, Online in 24/7, Support command : Moderation, Music etc.**')
   message.channel.send(stats)
- });
-};
+ };
 
 
 function parseDur (ms){
