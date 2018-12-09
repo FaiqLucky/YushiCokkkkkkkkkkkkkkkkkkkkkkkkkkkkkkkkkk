cf33ba584d0896357efdbdfddab9fd4a35dde556
LIASBDLIASHH9Q3PHB412B3412IB3123 <43847033+LIASBDLIASHH9Q3PHB412B3412IB3123@users.noreply.github.com>
2018-12-09 15:59:32 +0700

    Create gifsearch.js

---
+++ src/commands/search/gifsearch.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const gifSearch = require("gif-search");
+
+exports.run = (bot, message, args) => {
+  
+
+    if (!args[0]) return message.channel.send("yu!gif <gname>`");
+
+    gifSearch.random(args[0]).then(
+        gifUrl => {
+
+        let randomcolor = ((1 << 24) * Math.random() | 0).toString(16) 
+        var embed = new Discord.RichEmbed()
+            .setColor(`#${randomcolor}`)
+            .setImage(gifUrl)
+        message.channel.send(embed);
+    });
+
+exports.conf = {
+    aliases: [],
+    cooldown: 10
+}
+
+exports.help = {
+  name: "gifsearch",
+  description: "To find gif image",
+  usage: "gifsearch <gifname>
+}
