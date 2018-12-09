d1edf8877408a9acd07bf599f90d00ecd31ef075
LIASBDLIASHH9Q3PHB412B3412IB3123 <43847033+LIASBDLIASHH9Q3PHB412B3412IB3123@users.noreply.github.com>
2018-12-09 13:34:54 +0700

    Update stats.js

--- src/commands/information/stats.js
+++ src/commands/information/stats.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const { RichEmbed, version } = require('discord.js');
 const moment = require("moment");
 require("moment-duration-format");
-const os = require("os");
+const os = require("os");Memor Usage : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
 const cpuStat = require("cpu-stat");
 
 exports.run = async (client, message, args) => {
@@ -21,7 +21,7 @@ exports.run = async (client, message, args) => {
   .addField("General Information:", `Uptime: ${uptime}\nShard: ${client.shard.count}/1\nWS Ping: ${client.ping.toFixed(2)}ms`, true)
   .addField("Create add:", `${moment.utc(client.user.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
   .addField("Connected to:", `${client.guilds.size} Guilds\n${client.users.size} Users\n${client.channels.size} Channels`,true)
-  .addField("System Status:", `Memor Usage : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\nDiscord.js : v${version}\nNode : ${process.version}\nCPU : ${os.cpus().map(i => i.model)[0]}\nCPU Usage : ${percent.toFixed(2)}%\nArch : ${os.arch()}\nPlatform : ${os.platform()}`, true)
+  .addField("System Status:", `Discord.js : v${version}\nNode : ${process.version}\nBot Version : 0.0.1-Beta\nMemor Usage : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\nCPU : ${os.cpus().map(i => i.model)[0]}\nCPU Usage : ${percent.toFixed(2)}%\nArch : ${os.arch()}\nPlatform : ${os.platform()}`, true)
    .addField('About me', '**Yushi Nishimaro bot created in Indonesian , High Quality Bot, Online in 24/7, Support command : Moderation, Music etc.**')
   message.channel.send(stats)
  });
