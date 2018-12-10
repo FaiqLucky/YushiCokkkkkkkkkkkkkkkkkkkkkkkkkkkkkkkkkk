dfee67d09938b46d4f5b998004f45d8039f1812d
LIASBDLIASHH9Q3PHB412B3412IB3123 <43847033+LIASBDLIASHH9Q3PHB412B3412IB3123@users.noreply.github.com>
2018-12-10 07:46:07 +0700

    Update exec.js

--- src/commands/developer/exec.js
+++ src/commands/developer/exec.js
@@ -16,3 +16,24 @@ exports.help = {
   description: "{Some super}",
   usage: "nga tau gw"
 }
+const { exec } = require('child_process');
+
+exports.run = async(client, msg, args) => {
+    if (msg.author.id !== '446941102315012097' && msg.author.id !== '242969117479403520') return msg.channel.send('Sorry, only my developers can use this')
+    exec(args.join(' '), (error, output) => {
+        if(!error){
+            return msg.channel.send(output, { code: 'bash'});
+        }
+        return msg.channel.send(error, { code: 'bash'});
+    });
+}
+
+exports.conf = {
+    aliases: ['exe'],
+  cooldown: 0
+}
+
+exports.help = {
+  name: "exec",
+  category: "UNDEFINED"
+}
