1b3396515e26360190415105d42e9393d192e9f8
LIASBDLIASHH9Q3PHB412B3412IB3123 <43847033+LIASBDLIASHH9Q3PHB412B3412IB3123@users.noreply.github.com>
2018-12-10 07:47:11 +0700

    Update exec.js

--- src/commands/developer/exec.js
+++ src/commands/developer/exec.js
@@ -1,24 +1,6 @@
 const { exec } = require('child_process');
 
 exports.run = async(client, msg, args) => {
-    if (msg.author.id !== '242969117479403520' && msg.author.id !== '242969117479403520') return msg.channel.send('Sorry, only my developers can use this')
-    exec(`${args.join(' ')}`, (error, stdout) => {
-      const response = (error || stdout);
-      message.channel.send(`Ran: ${args.join(" ")}\n${response}`, {code: "asciidoc", split: "\n"}).catch(console.error);
-    });
-};
-exports.conf = {
-  aliases: ["ex"],
- cooldown: 0
-}
-exports.help = {
-  name: "exec",
-  description: "{Some super}",
-  usage: "nga tau gw"
-}
-const { exec } = require('child_process');
-
-exports.run = async(client, msg, args) => {
     if (msg.author.id !== '446941102315012097' && msg.author.id !== '242969117479403520') return msg.channel.send('Sorry, only my developers can use this')
     exec(args.join(' '), (error, output) => {
         if(!error){
