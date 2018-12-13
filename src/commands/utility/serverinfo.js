7e3a38ee8b20484302e9336bed18033bb1f1a392
LIASBDLIASHH9Q3PHB412B3412IB3123 <43847033+LIASBDLIASHH9Q3PHB412B3412IB3123@users.noreply.github.com>
2018-12-13 10:01:12 +0700

    Update serverinfo.js

--- src/commands/utility/serverinfo.js
+++ src/commands/utility/serverinfo.js
@@ -1,4 +1,21 @@
 const Discord =  require('discord.js');
+var verificationLevels = ['**None**', '**Low**', '**Medium**', '**(╯°□°）╯︵ ┻━┻** (High)', '**┻━┻彡 ヽ(ಠ益ಠ)ノ彡┻━┻** (Extreme)'];
+var region = {
+    "brazi": "Brazil",
+    "eu-central": "Central Europe",
+    "singapore": "Singapore",
+    "us-central": "U.S. Central",
+    "sydney": "Sydney",
+    "us-east": "U.S. East",
+    "us-south": "U.S. South",
+    "us-west": "U.S. West",
+    "eu-west": "Western Europe",
+    "singapore": "Singapore",
+    "london": "London",
+    "japan": "Japan",
+    "russia": "Russia",
+    "hongkong": "Hong Kong"
+  }
 
 exports.run = async (client, message, args) => {
   let si = message.guild.iconURL;
@@ -6,6 +23,8 @@ exports.run = async (client, message, args) => {
   .setAuthor(`${message.guild.name}`, si)
   .setThumbnail(si)
   .setDescription(`**ID:** \`${message.guild.id}\`\n**Guild Owner:** ${message.guild.owner.user.tag}`)
+  .addField("Guild Verification Levels:", `${verificationLevels[message.guild.verificationLevel]}`, true)
+  .addField("Guild Region:", `${region[message.guild.region]}`, true)
   
   message.channel.send(se);
 };
