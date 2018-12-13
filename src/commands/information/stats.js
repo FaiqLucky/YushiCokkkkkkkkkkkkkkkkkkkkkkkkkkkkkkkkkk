35e2e09737968db883ee26dcea88e8ce3435853a
LIASBDLIASHH9Q3PHB412B3412IB3123 <43847033+LIASBDLIASHH9Q3PHB412B3412IB3123@users.noreply.github.com>
2018-12-13 13:27:16 +0700

    Update stats.js

--- src/commands/information/stats.js
+++ src/commands/information/stats.js
@@ -18,7 +18,9 @@ exports.run = async (client, message, args) => {
   .addField("Connected to:", `${client.guilds.size} Guilds\n${client.users.size} Users\n${client.channels.size} Channels`,true)
   //.addField("CPU Status:", `CPU : ${os.cpus().map(i => i.model)[0]}\nArch : ${os.arch()}\nPlatform : ${os.platform()}`)
   .addField("System Status:", `Discord.js : v${version}\nNode : ${process.version}\nBot Version : 0.0.1-Beta\nMemor Usage : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB`, true)
-   .addField('About me', '**Yusa Nishimaro bot created in Indonesian , High Quality Bot, Online in 24/7, Support command : Moderation, Music etc.**')
+  .addField('About me', '**Yusa Nishimaro bot created in Indonesian , High Quality Bot, Online in 24/7, Support command : Moderation, Music etc.**')
+  .setFooter(`Request For: ${message.author.tag}`)
+  .setTimestamp()
   message.channel.send(stats)
  };
 
