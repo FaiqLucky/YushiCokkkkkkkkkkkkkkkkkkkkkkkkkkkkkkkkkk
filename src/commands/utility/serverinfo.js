38e7004de1ebce724d77feb624554f03b9d3112f
LIASBDLIASHH9Q3PHB412B3412IB3123 <43847033+LIASBDLIASHH9Q3PHB412B3412IB3123@users.noreply.github.com>
2018-12-13 14:12:59 +0700

    Update serverinfo.js

--- src/commands/utility/serverinfo.js
+++ src/commands/utility/serverinfo.js
@@ -21,13 +21,13 @@ var region = {
  
 
 exports.run = async (client, message, args) => {
-  let emojis;
   let cate = message.guild.channels.filter(x=>x.type ==='category').size;
   let txt = message.guild.channels.filter(x=>x.type ==='text').size;
   let vc = message.guild.channels.filter(x=>x.type ==='voice').size;
   let si = message.guild.iconURL;
   let se = new Discord.RichEmbed()
-  let (message.guild.emojis.size === 0) return message.channel.send(`emojis`, `There are no emojis on this server.`);
+  let emo = (message.guild.emojis.size === 0) return message.channel.send(`There are no emojis on this server.`);
+  let emoji = message.guild.emojis.map(e => e).join(' ');
   .setAuthor(`${message.guild.name}`, si)
   .setThumbnail(si)
   .setDescription(`**ID:** \`${message.guild.id}\`\n**Guild Owner:** ${message.guild.owner.user.tag}`)
@@ -37,7 +37,7 @@ exports.run = async (client, message, args) => {
   .addField(`Members [ ${message.guild.members.size} ]`, `${message.guild.members.filter(o => o.presence.status === 'online').size} Online\n${message.guild.members.filter(i => i.presence.status === 'idle').size} Idle\n${message.guild.members.filter(dnd => dnd.presence.status === 'dnd').size} Dnd\n${message.guild.members.filter(off => off.presence.status === 'offline').size} Offline\Invisible`)  
   .addField(`Channels [ ${message.guild.channels.size} ]`, `${cate} Categories\n ${txt} Text\n ${vc} Voice`)
   .addField(`Roles [ ${message.guild.roles.size} ]`, `To see list role use **yu!serverroles/yu!srl**`)
-  .addField(`Emojis [ ${message.guild.emojis.size} ]`, `${emojis} ${message.guild.emojis.map(e => e).join(' ')}`)
+  .addField(`Emojis [ ${message.guild.emojis.size} ]`, `${emo} ${emoji}`)
   
   message.channel.send(se);
 };
