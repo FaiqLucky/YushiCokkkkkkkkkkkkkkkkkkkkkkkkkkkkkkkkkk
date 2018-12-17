d0ef1bc7c7efc9a50b1cc1eae5dea7a3d66f29b0
LIASBDLIASHH9Q3PHB412B3412IB3123 <43847033+LIASBDLIASHH9Q3PHB412B3412IB3123@users.noreply.github.com>
2018-12-17 11:03:18 +0700

    Create invite.js

---
+++ src/commands/information/invite.js
@@ -0,0 +1,21 @@
+const { RichEmbed} = require('discord.js'}
+
+exports.run = async (client, message, args) => {
+let ic = client.user.avatarURL;
+let inv = new RichEmbed()
+.setThumbnail(ic)
+.setAuthor(`${client.user.tag}Invite`, ic)
+.setDescription("Invite me to your guild", `[Invite me](http://bit.ly/YusaInvite)`)
+message.channel.send(inv)
+}
+
+exports.conf = {
+  alises: ['inv'],
+  cooldown: 5
+  }
+  
+exports.help = {
+name: 'invite',
+description: 'Invite me to your server'
+usage: 'invite/inv'
+}
