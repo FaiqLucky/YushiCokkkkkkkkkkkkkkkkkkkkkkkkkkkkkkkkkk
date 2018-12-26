22f80d7f2b9e9804ab5b4a9a3e10652dac8751fd
LIASBDLIASHH9Q3PHB412B3412IB3123 <43847033+LIASBDLIASHH9Q3PHB412B3412IB3123@users.noreply.github.com>
2018-12-26 10:07:48 +0700

    Update message.js

--- src/events/message.js
+++ src/events/message.js
@@ -1,6 +1,10 @@
 const PREFIX = require('../config.json').bot_prefix;
+const db = require('quick.db');
 
 module.exports = async (client, message) => {
+    db.add(`globalMessage_${message.author.id}`, 1);
+    db.add(`guildMessage_${message.guild.id}_${message.author.id}`, 1);
+    
     if (message.author.bot || !message.guild) return;
 
     let prefix = PREFIX.toLowerCase();
