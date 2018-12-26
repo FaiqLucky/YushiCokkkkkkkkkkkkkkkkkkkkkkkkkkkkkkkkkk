5ce9f1f98d2340c2be1a53fdaa33f980d6bee96f
LIASBDLIASHH9Q3PHB412B3412IB3123 <43847033+LIASBDLIASHH9Q3PHB412B3412IB3123@users.noreply.github.com>
2018-12-26 10:26:48 +0700

    Update message.js

--- src/commands/utility/message.js
+++ src/commands/utility/message.js
@@ -2,20 +2,21 @@ const { RichEmbed } = require('discord.js');
 const db = require('quick.db');
 
 exports.run = async (client, args, message) => {
-    let user;
+   
+  let user;
     if (message.mentions.users.first()) {
       user = message.mentions.users.first();
     } else {
         user = message.author;
     }
-    
- let global = await db.fect(`globalMessage_${user.id}`)
- let guild = await db.fect(`guildMessage_${message.guild.id}_${user.id}`)
- 
- let msg = new RichEmbed()
-   .setDescription(`GlobalMessage: ${global}\nGuildMessage: ${guild}`)
- message.channel.send(msg)
- 
+
+  
+  let global = await db.fetch(`globalMessage_${user.id}`);
+  let guild = await db.fetch(`guildMessage_${message.guild.id}_${user.id}`);
+  
+  let msg = new RichEmbed()
+  .setDescription(`Global Message: ${global}\nGuildMessage: ${guild}`)
+  message.channel.send(msg)
 }
 
 exports.conf = {
